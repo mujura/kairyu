51317e230713cae67ba5d86a88e51a06aa6d0edc
ムジュラ <125709069+mujura@users.noreply.github.com>
2023-02-18 23:17:32 -0500

    Effect treated as a normal monster

--- script/c766341490.lua
+++ script/c766341490.lua
@@ -2,6 +2,7 @@
 Duel.LoadScript("script.lua")
 local s, id = GetID()
 function s.initial_effect(c)
+    Script.TreatedAsNormalMonster(c, LOCATION_HAND + LOCATION_GRAVE)
 end
 
 s.listed_names = { 22702055 }
--- script/script.lua
+++ script/script.lua
@@ -1,1 +1,15 @@
+Script = {}
 
+function Script.TreatedAsNormalMonster(c, location)
+    local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e1:SetCode(EFFECT_ADD_TYPE)
+    e1:SetRange(location)
+    e1:SetValue(TYPE_NORMAL)
+    c:RegisterEffect(e1)
+    local e2 = e1:Clone()
+    e2:SetCode(EFFECT_REMOVE_TYPE)
+    e2:SetValue(TYPE_EFFECT)
+    c:RegisterEffect(e2)
+end
